2ccc4b388ffff95857fa95086e0ccb03c8061b99
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-01-08 09:18:30 -0300

    feat: css properties

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,38 @@
-import { ComponentProps } from 'react';
+import { CSSProperties, ComponentProps } from 'react';
 
 type InputProps = ComponentProps<'input'> & {
   label: string;
   id: string;
 };
 
+const generalStyle: CSSProperties = {
+  fontSize: '1rem',
+  fontWeight: 600,
+  color: 'var(--color-2)',
+  padding: 'var(--gap-s) .75rem',
+  backgroundColor: 'var(--color-4)',
+  borderRadius: 'var(--gap)',
+};
+
+const labelStyle: CSSProperties = {
+  display: 'block',
+  marginBottom: 'var(--gap-s)',
+  ...generalStyle,
+};
+
+const inputStyle: CSSProperties = {
+  border: 'none',
+  fontFamily: 'monospace',
+  ...generalStyle,
+};
+
 export const Input = ({ label, id, ...props }: InputProps) => {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} name={id} {...props} />
+      <label style={labelStyle} htmlFor={id}>
+        {label}
+      </label>
+      <input style={inputStyle} type="text" id={id} name={id} {...props} />
     </div>
   );
 };
